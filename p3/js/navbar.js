35a5a21342170d97590746c49523a9832cf51f3b
hannamoe <moeehanna@gmail.com>
2020-11-03 11:59:10 +0100

    joho

--- p3/js/navbar.js
+++ p3/js/navbar.js
@@ -1,15 +1,5 @@
 
 
-/*window.onscroll = function() {scrollFunction()};
-
-
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    document.querySelector(".topnav").style.top = "0";
-  } else {
-    document.querySelector(".topnav").style.top = "-90px";
-  }
-}*/
 let prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
   let currentScrollPos = window.pageYOffset;
