47aa9d99d196c5f570a143ea82156f43f1de4a8a
ingridowren <72023935+ingridowren@users.noreply.github.com>
2020-11-03 15:13:12 +0100

    about introvideo

---
+++ p3/js/video.js
@@ -0,0 +1,15 @@
+function loadVideo() {
+  const videoplayer = document.getElementById("videoPlayer");
+  const videoEl = document.getElementById("introvideo");
+  videoplayer.appendChild(videoEl);
+  videoplayer.style.display = "block";
+}
+/*
+function on() {
+document.getElementById("overlay").style.display = "block";
+}
+
+function off() {
+document.getElementById("overlay").style.display = "none";
+}
+*/
