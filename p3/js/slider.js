4829fb3402bfa6d85817d23ade08a53c36948a8f
Herman <herman2304@hotmail.com>
2020-11-04 13:32:44 +0100

    close-button

--- p3/js/slider.js
+++ p3/js/slider.js
@@ -16,7 +16,12 @@ let arrowLeft = document.querySelector("#arrow-left");
 let arrowRight = document.querySelector("#arrow-right");
 let current = 0;
 
-    // Klarere alle bilder:
+// Close-button:
+function goBack() {
+    window.history.back();
+  }
+
+// Klarere alle bilder:
 function reset(){
     for(let i = 0; i < sliderImages.length; i++){
         sliderImages[i].style.display = "none";
