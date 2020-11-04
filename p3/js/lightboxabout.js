744f3a2c15aee53b6696d0fa66d3421fb3c74670
hannamoe <moeehanna@gmail.com>
2020-11-04 18:18:55 +0100

    joho

--- p3/js/lightboxabout.js
+++ p3/js/lightboxabout.js
@@ -1,28 +1,27 @@
-const lightbox = document.createElement("div");
-lightbox.id = "lightbox";
-document.body.appendChild(lightbox);
+const lightbox = document.createElement("div"); //lager et div-element som kalles lightbox
+lightbox.id = "lightbox"; //gir elementet en id
+document.body.appendChild(lightbox); //legger til elementet i body i htmlen
 
 function makeLightbox(){
-    const images = document.querySelectorAll("img");
-    images.forEach(image => {
-        image.addEventListener("click", e =>{
-            if (image.id != "brage"){
-                lightbox.classList.add("active")
-                const img = document.createElement("img");
-                img.src = image.src;
-                while (lightbox.firstChild){
-                    lightbox.removeChild(lightbox.firstChild);
-                }
-                lightbox.appendChild(img); 
-            }
-        
-        })
-    })
+    const images = document.querySelectorAll("img"); //henter ut alle img elementene og legger de i en nodelist 
+    images.forEach(image => { //forEach-funksjonen går gjennom alle img elementene i images 
+        image.addEventListener("click", e =>{ //dersom man klikker på et image skal et event skje 
+            if (image.id != "brage"){ //funksjonen skal ikke skje på bilde av brage, og utelukker derfor det med en if-setning som sier at hvis id'en til image er ulik brage så skal funksjonen kjøre videre
+                lightbox.classList.add("active")//bruker classlist for å manipulere css, her addes en css class kalt "active" til lightbox elementet 
+                const img = document.createElement("img"); //lager et img-element 
+                img.src = image.src; //setter src, altså url'en, til det nye img-elementet lik det gamle
+                while (lightbox.firstChild){ //så lenge det er et firstChild i lightbox 
+                    lightbox.removeChild(lightbox.firstChild); //fjerner firstchild elementet 
+                };
+                lightbox.appendChild(img); //legger til det nye img-elemntet
+            };
+        });
+    });
 
-    lightbox.addEventListener("click", e => {
-        if (e.target !== e.currentTarget) return
-        lightbox.classList.remove("active");
+    lightbox.addEventListener("click", e => { //når man klikker på lightboxen skal et event skje
+        if (e.target !== e.currentTarget) return //sører for at lightboxen kun avsluttes om man trykker utenfor bilde
+            lightbox.classList.remove("active"); //fjerner css class "active" som ble lagd i stad
     })
-}
+};
 
 makeLightbox();
