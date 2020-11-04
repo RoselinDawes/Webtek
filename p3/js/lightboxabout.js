f44db3972f64e29603cbc7bf3c01323d8ef2f108
hannamoe <moeehanna@gmail.com>
2020-11-04 10:32:36 +0100

    joho

--- p3/js/lightboxabout.js
+++ p3/js/lightboxabout.js
@@ -2,22 +2,27 @@ const lightbox = document.createElement("div");
 lightbox.id = "lightbox";
 document.body.appendChild(lightbox);
 
-
-const images = document.querySelectorAll(".bilde");
-images.forEach(image => {
-    image.addEventListener("click", e =>{
-        lightbox.classList.add("active")
-        const img = document.createElement("img");
-        img.src = image.src;
-        while (lightbox.firstChild){
-            lightbox.removeChild(lightbox.firstChild);
-        }
-        lightbox.appendChild(img);
-
+function makeLightbox(){
+    const images = document.querySelectorAll("img");
+    images.forEach(image => {
+        image.addEventListener("click", e =>{
+            if (image.id != "brage"){
+                lightbox.classList.add("active")
+                const img = document.createElement("img");
+                img.src = image.src;
+                while (lightbox.firstChild){
+                    lightbox.removeChild(lightbox.firstChild);
+                }
+                lightbox.appendChild(img); 
+            }
+        
+        })
     })
-})
 
-lightbox.addEventListener("click", e => {
-    if (e.target !== e.currentTarget) return
-    lightbox.classList.remove("active");
-})
+    lightbox.addEventListener("click", e => {
+        if (e.target !== e.currentTarget) return
+        lightbox.classList.remove("active");
+    })
+}
+
+makeLightbox();
