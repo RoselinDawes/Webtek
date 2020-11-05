3efbd7db517580c6649a9d9a68188a61b69b8902
hannamoe <moeehanna@gmail.com>
2020-11-05 21:28:54 +0100

    joho

--- BrageBakke/js/submit.js
+++ BrageBakke/js/submit.js
@@ -1,3 +1,12 @@
 function validateSubmit(){
+  const fname = document.forms["myForm"]["fname"].value
+  const lname = document.forms["myForm"]["lname"].value
+  const email = document.forms["myForm"]["email"].value
+  if ((fname && lname && email) == ""){ //sørger for at man ikke kan submitte et form uten å ha skrevet mail og navn
+    window.alert("You must fill out the form") 
+  }else{
     window.alert("Your request was submitted, thank you!") //gir en feedback via et alert-vindu 
-  };
+  }
+};
+
+
